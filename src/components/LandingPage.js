9ee0eb0c7cbcc19135fe3a6bc1aeb15558868793
Jillo Woche <jillo.abdulla@gmail.com>
2019-10-19 20:30:48 +0300

    Change background images

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -28,9 +28,6 @@ const LandingPage = () => {
                 <div className="header-button">
                     <a className="btn-project" href="#portfolio-section">see projects</a>
                 </div>
-                <div className="logo">
-                    <img className="logo-image" src="images/logo.png" alt="logo"/>
-                </div>
             </header>
             <section id="about-section">
                 <div className="heading">
@@ -40,13 +37,16 @@ const LandingPage = () => {
                     <div className="row">
                         <div className="description col s12 l6">
                             <h3 className="heading-tertiary">motivated to produce results</h3>
-                            <p className="paragraph">As a tenacious self-taught programmer, I use continuous iteration to produce results quickly and continuously improve products.</p>
+                            <p className="paragraph">As a tenacious self-taught programmer, 
+                            I use continuous iteration to produce results quickly and continuously improve products.</p>
                             <h3 className="heading-tertiary">an agile collaborator</h3>
-                            <p className="paragraph">I have contributed to open source projects, worked on engineering teams, and always stay attuned to the newest frameworks.</p>
+                            <p className="paragraph">I have contributed to open source projects, 
+                            worked on engineering teams, and always stay attuned to the newest frameworks.</p>
                         </div>
                         <div className="images col s12 l6">
                             <a href="https://www.python.org/" target="_blank" rel="noopener noreferrer"><img src="images/python.png" alt="python"/></a>
-                            <a href="https://en.wikipedia.org/wiki/JavaScript" target="_blank" rel="noopener noreferrer"><img src="images/javascript.png" alt="javasript"/></a>
+                            <a href="https://en.wikipedia.org/wiki/JavaScript" 
+                            target="_blank" rel="noopener noreferrer"><img src="images/javascript.png" alt="javasript"/></a>
                             <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer"><img src="images/react.png" alt="react"/></a>
                             <a href="https://www.djangoproject.com/" target="_blank" rel="noopener noreferrer"><img src="images/django.png" alt="django"/></a>
                         </div>

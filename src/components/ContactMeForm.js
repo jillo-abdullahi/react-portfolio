0749779e0ffa73e03df46f7e902b89ee114b74d9
jillo-abdullahi <jillo.abdulla@gmail.com>
2024-05-23 16:44:27 +0300

    bug: fix sending email through emailJS

--- src/components/ContactMeForm.js
+++ src/components/ContactMeForm.js
@@ -1,9 +1,48 @@
 import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
+import emailjs from "emailjs-com";
+import { useState } from "react";
 import ContactLinks from "./ContactLinks";
 
 export default function ContactMeForm() {
+  const initialFormData = {
+    name: "",
+    email: "",
+    message: "",
+  };
+  const [formData, setFormData] = useState(initialFormData);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData((prevState) => {
+      return {
+        ...prevState,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        formData,
+        process.env.REACT_APP_PUBLIC_KEY
+      )
+      .then(
+        (response) => {
+          setFormData(initialFormData);
+        },
+        (error) => {
+          console.log({ error });
+        }
+      );
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 space-x-0 sm:space-x-4 justify-center pt-6">
         {/* contact form  */}
         <div className="w-full sm:w-1/2 space-y-3">
@@ -13,7 +52,9 @@ export default function ContactMeForm() {
                 type="text"
                 name="name"
                 id="name"
+                onChange={handleChange}
                 placeholder="Name"
+                required
                 autoComplete="given-name"
                 className="block w-full rounded-md border-0 bg-white/5 py-3 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-orange/90 font-bold sm:text-sm sm:leading-6"
               />
@@ -25,6 +66,8 @@ export default function ContactMeForm() {
                 name="email"
                 type="email"
                 placeholder="Email"
+                onChange={handleChange}
+                required
                 autoComplete="email"
                 className="block w-full rounded-md border-0 bg-white/5 py-3 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-orange/90 font-bold sm:text-sm sm:leading-6"
               />
@@ -35,6 +78,8 @@ export default function ContactMeForm() {
             <textarea
               id="message"
               name="message"
+              onChange={handleChange}
+              required
               rows={5}
               placeholder="Your message"
               className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-orange/90 font-bold sm:text-sm sm:leading-6"

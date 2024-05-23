561b6ed8fa2a5813e98848b36a4e48d879082203
jillo-abdullahi <jillo.abdulla@gmail.com>
2024-05-23 21:58:00 +0300

    feat: show button text along with icon

--- src/components/shared/buttonGroup.js
+++ src/components/shared/buttonGroup.js
@@ -5,25 +5,27 @@ export default function ButtonGroup({ link1, link2 }) {
     <span className="isolate inline-flex rounded-md shadow-sm">
       <a
         type="button"
-        className="relative inline-flex items-center gap-x-1.5 rounded-l-md px-3 py-2 text-sm font-semibold focus:z-10 bg-orange/90 capitalize text-blue focus:bg-orange group"
+        className="relative inline-flex items-center gap-x-1.5 rounded-l-md px-3 py-2 text-sm font-semibold focus:z-10 bg-orange/90 text-blue focus:bg-orange group"
         href={link1}
         target="_blank"
       >
         <IconScanEye
-          className="h-5 w-5 text-blue transition-all duration-150 group-hover:scale-110"
+          className="h-5 w-5 text-blue transition-all duration-150 group-hover:scale-110 inline"
           aria-hidden="true"
         />
+        Preview
       </a>
       <a
         type="button"
-        className="group relative -ml-px inline-flex items-center rounded-r-md px-3 py-2 text-sm font-semibold focus:z-10 bg-orange/80 capitalize text-blue focus:bg-orange"
+        className="group relative -ml-px inline-flex items-center rounded-r-md px-3 py-2 text-sm font-semibold focus:z-10 bg-orange/80 text-blue focus:bg-orange"
         href={link2}
         target="_blank"
       >
         <IconBrandGithub
-          className="h-5 w-5 text-blue transition-all duration-150 group-hover:scale-110"
+          className="h-5 w-5 text-blue transition-all duration-150 group-hover:scale-110 inline mr-1"
           aria-hidden="true"
         />
+        Source code
       </a>
     </span>
   );

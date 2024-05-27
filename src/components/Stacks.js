166e70c3870be202433938cbf25b940363b9f48e
jillo-abdullahi <jillo.abdulla@gmail.com>
2024-05-27 22:29:37 +0300

    feat: animate stack cards individually

--- src/components/Stacks.js
+++ src/components/Stacks.js
@@ -7,43 +7,40 @@ import {
 } from "@heroicons/react/24/outline";
 import { skills } from "../portfolioData";
 import { StackCard } from "./shared/stackCard";
-import { SlideMeIn } from "./shared/slideMeIn";
 
 const Stacks = () => {
   return (
     <div className="content">
-      <SlideMeIn>
-        <div className="grid grid-cols-12 gap-3">
-          <div className="col-span-12 md:col-span-6 lg:col-span-3">
-            <StackCard
-              icon={<ComputerDesktopIcon className="text-orange w-8 h-8" />}
-              title="front-end"
-              skills={skills.frontend}
-            />
-          </div>
-          <div className="col-span-12 md:col-span-6 lg:col-span-3">
-            <StackCard
-              icon={<ServerStackIcon className="text-orange w-8 h-8" />}
-              title="back-end"
-              skills={skills.backend}
-            />
-          </div>
-          <div className="col-span-12 md:col-span-6 lg:col-span-3">
-            <StackCard
-              icon={<CircleStackIcon className="text-orange w-8 h-8" />}
-              title="databases"
-              skills={skills.databases}
-            />
-          </div>
-          <div className="col-span-12 md:col-span-6 lg:col-span-3">
-            <StackCard
-              icon={<WrenchScrewdriverIcon className="text-orange w-8 h-8" />}
-              title="devops"
-              skills={skills.devops}
-            />
-          </div>
+      <div className="grid grid-cols-12 gap-3">
+        <div className="col-span-12 md:col-span-6 lg:col-span-3">
+          <StackCard
+            icon={<ComputerDesktopIcon className="text-orange w-8 h-8" />}
+            title="front-end"
+            skills={skills.frontend}
+          />
         </div>
-      </SlideMeIn>
+        <div className="col-span-12 md:col-span-6 lg:col-span-3">
+          <StackCard
+            icon={<ServerStackIcon className="text-orange w-8 h-8" />}
+            title="back-end"
+            skills={skills.backend}
+          />
+        </div>
+        <div className="col-span-12 md:col-span-6 lg:col-span-3">
+          <StackCard
+            icon={<CircleStackIcon className="text-orange w-8 h-8" />}
+            title="databases"
+            skills={skills.databases}
+          />
+        </div>
+        <div className="col-span-12 md:col-span-6 lg:col-span-3">
+          <StackCard
+            icon={<WrenchScrewdriverIcon className="text-orange w-8 h-8" />}
+            title="devops"
+            skills={skills.devops}
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/shared/stackCard.js
+++ src/components/shared/stackCard.js
@@ -1,28 +1,32 @@
+import { SlideMeIn } from "./slideMeIn";
+
 export const StackCard = ({ title, icon, skills }) => {
   return (
-    <div className="h-auto my-5 p-8 text-center rounded-lg border border-gray-900 bg-[#272f3790] hover:-translate-y-2 transition-all duration-300 text-base">
-      <div className="flex items-center justify-start space-x-2 mb-6">
-        {icon}
-        <span className="text-gray-200 uppercase font-bold text-base">
-          {title}
-        </span>
-      </div>
+    <SlideMeIn cascade={true}>
+      <div className="h-auto my-5 p-8 text-center rounded-lg border border-gray-900 bg-[#272f3790] hover:-translate-y-2 transition-all duration-300 text-base">
+        <div className="flex items-center justify-start space-x-2 mb-6">
+          {icon}
+          <span className="text-gray-200 uppercase font-bold text-base">
+            {title}
+          </span>
+        </div>
 
-      <div className="flex flex-col space-y-3">
-        {skills.map(({ name, level }) => (
-          <div className="flex flex-col items-start space-y-1" key={name}>
-            <div className="text-gray-200 w-full flex items-center justify-between">
-              <span className="text-base">{name}</span>
+        <div className="flex flex-col space-y-3">
+          {skills.map(({ name, level }) => (
+            <div className="flex flex-col items-start space-y-1" key={name}>
+              <div className="text-gray-200 w-full flex items-center justify-between">
+                <span className="text-base">{name}</span>
+              </div>
+              <div className="w-full h-1 bg-gray-300">
+                <div
+                  className="bg-orange/90 h-full"
+                  style={{ width: `${level}` }}
+                ></div>
+              </div>
             </div>
-            <div className="w-full h-1 bg-gray-300">
-              <div
-                className="bg-orange/90 h-full"
-                style={{ width: `${level}` }}
-              ></div>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </SlideMeIn>
   );
 };
